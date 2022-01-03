3874249a7542351f2df44cf60283d726b70b4be5
mrnakumar <nknkchoudhary@gmail.com>
2022-01-03 15:09:15 +0100

    Format all Python code with 'black'
    Reformat all Python code to a standard format using the black [1]
    formatter.
    
    Most of the reformatting is related to removing trailing whitespaces
    in comments and multi value assignment.
    
    The formatting was done by executing:
    
    $ git ls-files | grep py$ | xargs black --fast
    
    Note that the --fast option is required to prevent the error:
    
    cannot use --safe with this file;
    failed to parse source file with Python 3.7's builtin AST.
    Re-run with --fast or stop using deprecated Python 2 syntax.
    AST error message: invalid syntax
    
    which is caused by usage of deprecated Python 2 syntax in several
    files.
    
    [1] https://github.com/psf/black

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -7,24 +7,34 @@ from Crypto import Random
 
 
 def encrypt(key, source, encode=True):
-    key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
+    key = SHA256.new(
+        key
+    ).digest()  # use SHA-256 over our key to get a proper-sized AES key
     iv = Random.new().read(AES.block_size)  # generate IV
     encryptor = AES.new(key, AES.MODE_CBC, iv)
     padding = AES.block_size - len(source) % AES.block_size  # calculate needed padding
     source += bytes([padding]) * padding  # Python 2.x: source += chr(padding) * padding
-    encrypted = iv + encryptor.encrypt(source)  # store the IV at the beginning and encrypt
+    encrypted = iv + encryptor.encrypt(
+        source
+    )  # store the IV at the beginning and encrypt
     return base64.b64encode(encrypted).decode("latin-1") if encode else encrypted
 
 
 def decrypt(key, source, decode=True):
     if decode:
         source = base64.b64decode(source.encode("latin-1"))
-    key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
-    iv = source[:AES.block_size]  # extract the IV from the beginning
+    key = SHA256.new(
+        key
+    ).digest()  # use SHA-256 over our key to get a proper-sized AES key
+    iv = source[: AES.block_size]  # extract the IV from the beginning
     decrypter = AES.new(key, AES.MODE_CBC, iv)
-    decrypted = decrypter.decrypt(source[AES.block_size:])  # decrypt
-    padding = decrypted[-1]  # pick the padding value from the end; Python 2.x: ord(data[-1])
-    if decrypted[-padding:] != bytes([padding]) * padding:  # Python 2.x: chr(padding) * padding
+    decrypted = decrypter.decrypt(source[AES.block_size :])  # decrypt
+    padding = decrypted[
+        -1
+    ]  # pick the padding value from the end; Python 2.x: ord(data[-1])
+    if (
+        decrypted[-padding:] != bytes([padding]) * padding
+    ):  # Python 2.x: chr(padding) * padding
         raise ValueError("Invalid padding...")
     return decrypted[:-padding]  # remove the padding
 
@@ -55,7 +65,7 @@ if __name__ == "__main__":
     else:
         target_file_path = folder + "/" + source_filename + ".ec"
 
-    with open(source_file_path, 'rb') as f:
+    with open(source_file_path, "rb") as f:
         source_contents = f.read()
         data = None
         if is_encrypt:
--- mailer.py
+++ mailer.py
@@ -17,7 +17,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
+
 
 def main():
     """Shows basic usage of the Gmail API.
@@ -28,7 +29,7 @@ def main():
     # Second: to user
     # Third: mail subject
     # Fourth: filepath to send in email
-    if len(sys.argv)!=5:
+    if len(sys.argv) != 5:
         print("Pass subject and filename")
         sys.exit(1)
     from_usr = sys.argv[1]
@@ -40,123 +41,124 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open("token.json", "w") as token:
             token.write(creds.to_json())
 
     try:
         # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        msg =  create_message_with_attachment(from_usr, to_usr, subject, '', filename)
-        send_message(service, 'me', msg)
+        service = build("gmail", "v1", credentials=creds)
+        msg = create_message_with_attachment(from_usr, to_usr, subject, "", filename)
+        send_message(service, "me", msg)
     except HttpError as error:
-        print(f'An error occurred: {error}')
+        print(f"An error occurred: {error}")
+
 
 def send_message(service, user_id, message):
-  """Send an email message.
+    """Send an email message.
 
-  Args:
-    service: Authorized Gmail API service instance.
-    user_id: User's email address. The special value "me"
-    can be used to indicate the authenticated user.
-    message: Message to be sent.
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      message: Message to be sent.
 
-  Returns:
-    Sent Message.
-  """
-  try:
-    message = (service.users().messages().send(userId=user_id, body=message)
-               .execute())
-    id = message['id']
-    print(f'Sent with id: {id}')
-    return message
-  except Exception as e:
-    print(f'An error occurred: {e}')
+    Returns:
+      Sent Message.
+    """
+    try:
+        message = (
+            service.users().messages().send(userId=user_id, body=message).execute()
+        )
+        id = message["id"]
+        print(f"Sent with id: {id}")
+        return message
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 
 def create_message(sender, to, subject, message_text):
-  """Create a message for an email.
+    """Create a message for an email.
 
-  Args:
-    sender: Email address of the sender.
-    to: Email address of the receiver.
-    subject: The subject of the email message.
-    message_text: The text of the email message.
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
 
-  Returns:
-    An object containing a base64url encoded email object.
-  """
-  message = MIMEText(message_text)
-  message['to'] = to
-  message['from'] = sender
-  message['subject'] = subject
-  as64 = base64.urlsafe_b64encode(message.as_bytes())
-  b64_string = as64.decode()
-  return {'raw': b64_string}
-
-def create_message_with_attachment(
-    sender, to, subject, message_text, file):
-  """Create a message for an email.
-
-  Args:
-    sender: Email address of the sender.
-    to: Email address of the receiver.
-    subject: The subject of the email message.
-    message_text: The text of the email message.
-    file: The path to the file to be attached.
-
-  Returns:
-    An object containing a base64url encoded email object.
-  """
-  message = MIMEMultipart()
-  message['to'] = to
-  message['from'] = sender
-  message['subject'] = subject
-
-  msg = MIMEText(message_text)
-  message.attach(msg)
-
-  content_type, encoding = mimetypes.guess_type(file)
-
-  if content_type is None or encoding is not None:
-    content_type = 'application/octet-stream'
-  main_type, sub_type = content_type.split('/', 1)
-  if main_type == 'text':
-    fp = open(file, 'rb')
-    msg = MIMEText(fp.read().decode("utf-8"), _subtype=sub_type)
-    fp.close()
-  elif main_type == 'image':
-    fp = open(file, 'rb')
-    msg = MIMEImage(fp.read(), _subtype=sub_type)
-    fp.close()
-  elif main_type == 'audio':
-    fp = open(file, 'rb')
-    msg = MIMEAudio(fp.read(), _subtype=sub_type)
-    fp.close()
-  else:
-    fp = open(file, 'rb')
-    print(main_type, sub_type)
-    msg = MIMEBase(main_type, sub_type)
-    msg.set_payload(fp.read())
-    fp.close()
-  filename = os.path.basename(file)
-  msg.add_header('Content-Disposition', 'attachment', filename=filename)
-  message.attach(msg)
-
-  as64 = base64.urlsafe_b64encode(message.as_bytes())
-  b64_string = as64.decode()
-  return {'raw': b64_string}
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message["to"] = to
+    message["from"] = sender
+    message["subject"] = subject
+    as64 = base64.urlsafe_b64encode(message.as_bytes())
+    b64_string = as64.decode()
+    return {"raw": b64_string}
 
 
-if __name__ == '__main__':
+def create_message_with_attachment(sender, to, subject, message_text, file):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
+      file: The path to the file to be attached.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart()
+    message["to"] = to
+    message["from"] = sender
+    message["subject"] = subject
+
+    msg = MIMEText(message_text)
+    message.attach(msg)
+
+    content_type, encoding = mimetypes.guess_type(file)
+
+    if content_type is None or encoding is not None:
+        content_type = "application/octet-stream"
+    main_type, sub_type = content_type.split("/", 1)
+    if main_type == "text":
+        fp = open(file, "rb")
+        msg = MIMEText(fp.read().decode("utf-8"), _subtype=sub_type)
+        fp.close()
+    elif main_type == "image":
+        fp = open(file, "rb")
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == "audio":
+        fp = open(file, "rb")
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(file, "rb")
+        print(main_type, sub_type)
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    filename = os.path.basename(file)
+    msg.add_header("Content-Disposition", "attachment", filename=filename)
+    message.attach(msg)
+
+    as64 = base64.urlsafe_b64encode(message.as_bytes())
+    b64_string = as64.decode()
+    return {"raw": b64_string}
+
+
+if __name__ == "__main__":
     main()
-
