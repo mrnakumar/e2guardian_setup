13ca8ec8e7a99804dd9292e3b7843ab044909a72
mrnakumar <nknkchoudhary@gmail.com>
2022-01-05 20:05:00 +0100

    Move crome sync and cron scripts in one file

--- cron_chrome_history_sync.sh
+++ cron_chrome_history_sync.sh
@@ -1,30 +1,134 @@
 #!/bin/bash
 
-# Script to setup cron job to schedule chrome history sync.
-# Should be run once on a computer.
-# If script finishes successfully then, doing: 'sudo crontab -u user -l' , should show the expected cron job
+# Based on requested operation (via command line argument), this script can do either:
+# 1. Setup cron job to send chrome history periodically
+# 2. Send chrome history
+# The setting up of cron job should be done only once on a computer.
+# The cron job will invoke the send chrome history function periodically thereafter.
+# If cron job setup finishes successfully then, doing: 'sudo crontab -u user -l' , should show the expected cron job
+#
 # Required:
 # Log directory /var/log/e2guardian should have permission 700.And should have file cron_chrome_sync.log.
 # To check for correctness, change the cron expression to run every minutes (i.e. '* * * * *'). Revert once checked.
 
-user="guest"
-workDir="/etc/${user}/chrome_history"
-sudo mkdir -p "${workDir}" || { echo "Failed to create workDir. Exiting."; exit; }
-sudo cp "./mailer.py" "./encrypt_decrypt.py" "./chrome-history-sync.sh" "./requirements-for-python-code.txt" "./token.json" "${workDir}/" || { echo "Failed to copy data/program files. Exiting."; exit; }
-currentDir="$PWD"
-cd "${workDir}" || { echo "Failed to cd into ${workDir}. Exiting"; exit ;}
-python3 -m venv venv || { "Failed to create python virtual enviornment. Exiting."; exit; }
-source "./venv/bin/activate"
-pip install -r "./requirements-for-python-code.txt" || { echo "Pip install failed for venv. Exiting."; exit; }
-cd "${currentDir}"  || { echo "Failed to cd into ${currentDir}. Exiting"; exit ;}
-sudo chown "${user}":"${user}" -R "${workDir}"
-sudo chmod 700 -R "${workDir}"
+function setup_cron_job(){
+    user="guest"
+    workDir="/etc/${user}/chrome_history"
+    sudo mkdir -p "${workDir}" || { echo "Failed to create workDir. Exiting."; exit; }
+    sudo cp "./mailer.py" "./encrypt_decrypt.py" "./chrome-history-sync.sh" "./requirements-for-python-code.txt" "./token.json" "${workDir}/" || { echo "Failed to copy data/program files. Exiting."; exit; }
+    currentDir="$PWD"
+    cd "${workDir}" || { echo "Failed to cd into ${workDir}. Exiting"; exit ;}
+    python3 -m venv venv || { "Failed to create python virtual enviornment. Exiting."; exit; }
+    source "./venv/bin/activate"
+    pip install -r "./requirements-for-python-code.txt" || { echo "Pip install failed for venv. Exiting."; exit; }
+    cd "${currentDir}"  || { echo "Failed to cd into ${currentDir}. Exiting"; exit ;}
+    sudo chown "${user}":"${user}" -R "${workDir}"
+    sudo chmod 700 -R "${workDir}"
 
-#write out current crontab
-sudo crontab -u "${user}" -l > mycron
-# Run every 20 minutes
-echo "*/20 * * * * ${workDir}/chrome-history-sync.sh '${user}' '{encryptKey}' '{fromEmail}' '{toEmail}' > /var/log/e2guardian/cron_chrome_sync.log 2>&1" >> mycron
-#install new cron file
-sudo crontab -u "${user}" mycron
-rm mycron
+    #write out current crontab
+    sudo crontab -u "${user}" -l > mycron
+    # Run every 20 minutes
+    echo "*/20 * * * * ${workDir}/chrome-history-sync.sh '${user}' '{encryptKey}' '{fromEmail}' '{toEmail}' > /var/log/e2guardian/cron_chrome_sync.log 2>&1" >> mycron
+    #install new cron file
+    sudo crontab -u "${user}" mycron
+    rm mycron
+}
 
+##########
+
+# Sync chrome usages. Needs sqlite3 in path
+function sync_chrome_history() {
+    if [ "$#" -ne 4 ] ; then
+        echo "Usage: $0 UserName EncryptionKey FromEmailAddress ToEmailAddress"
+        exit 1
+    fi
+
+    # user is used to construct path to chrome History file and workDir
+    user="$1"
+    ecKey="$2"
+    fromAddr="$3"
+    toAddr="$4"
+
+    LOCKFILE="/tmp/chrome_sync_lock.txt"
+    if [ -e ${LOCKFILE} ]; then
+        echo "already running"
+        exit
+    fi
+    # make sure the lockfile is removed when we exit and then claim it
+    trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
+    touch ${LOCKFILE}
+
+
+    # Business logic starts...
+    workDir="/etc/${user}/chrome_history"
+    cd ${workDir} || { echo "Could not cd into ${workDir}. Exiting"; exit; }
+    source "./venv/bin/activate" || { echo "Could not activate python venv.";  exit; }
+    ENCRYPT_UTIL="./encrypt_decrypt.py"
+    MAILER_UTIL="./mailer.py"
+    query="SELECT urls.url, urls.visit_count, urls.last_visit_time FROM urls;"
+    historyFileName="History"
+    historyFilePath="/home/${user}/.config/google-chrome/Default/${historyFileName}"
+    cp ${historyFilePath} ./${historyFileName}
+    day=$(date +%d)
+    recordsFilePrefix="records_"
+    recordFile="${recordsFilePrefix}${day}"
+    if [ ! -f "$recordFile" ]; then
+        # Check if previous day's record exist. If yes, then share those.
+        for fileName in `ls -d ${recordsFilePrefix}*`; do
+            filePath="${PWD}/${fileName}"
+            echo "Processing file $filePath"
+            # Send and then delete
+            # First: encrypt, and then send
+            if [ ! -s "$filePath" ]; then
+              echo "Empty file ${filePath}. Deleting and skipping send"
+              rm -f "$filePath"
+              continue
+            fi
+            python ${ENCRYPT_UTIL} "${ecKey}" "encrypt" "$filePath"
+            encryptedFilePath="${filePath}.ec"
+            if [ ! -f "$encryptedFilePath" ]; then
+                echo "Unknown error. Skipping sync of ${filePath}"
+                continue
+            else
+                # Send the encrypted file and then delete it.
+                echo "Sending file ${encryptedFilePath} ..."
+                python ${MAILER_UTIL} "${fromAddr}" "${toAddr}" "GChrme Records ${recordFile}" "${encryptedFilePath}"
+                rm -f "$encryptedFilePath" || { echo "Failed to delete ${encryptedFilePath}, exiting."; exit; }
+            fi
+            rm -f "$filePath"
+        done
+
+        touch "${recordFile}" || { echo "Failed to create records file ${recordFile}"; exit; }
+    fi
+    sqlite3 "./${historyFileName}" "${query}" >> "${recordFile}"
+    # Deduplicate records by url.
+    sort -u -t '|' -k1,1 "${recordFile}" -o "${recordFile}"
+    rm -f "./${historyFileName}"
+    rm -f "./${historyFileName}"
+    # Business logic ends here
+
+
+    rm -f ${LOCKFILE}
+}
+
+# Main logic begins here. Interpret and call appropriate function.
+if [ "$#" -lt 2 ]; then
+    echo "Usages: operation<setup_cron|sync_chrome> user"
+    exit 2
+fi
+operation="$1"
+# Inspect operation and call the corresponding function
+if [ "$operation" == "sync_chrome" ]; then
+    if [ "$#" -lt 5 ]; then
+        echo "Usages: Usages: Operation<setup_cron|sync_chrome> User EncryptionKey FromEmailAddress ToEmailAddress"
+        exit 1
+    fi
+    sync_chrome_history "$2" "$3" "$4" "$5"
+else
+    if [ "$operation" == "setup_cron" ]; then
+        setup_cron_job "$2"
+    else
+        echo "Invalid operation. Exiting."
+        exit 1
+    fi
+fi
