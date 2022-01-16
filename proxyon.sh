d27c7ace396b6d7aef5a4a594642329c60784831
mrnakumar <nknkchoudhary@gmail.com>
2022-01-16 14:07:02 +0100

    Add utility scripts to set/unset proxy
    Two scripts are added to set or unset system
    wide proxy on LUbuntu from command line.
    
    To set up localhost's port number 8080 as proxy,
    use the following command:
    ./proxyon.sh 127.0.0.1 8080
    
    To unset system wide proxy, use the following command:
    ./proxyoff.sh

---
+++ proxyon.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Can be used to setup system wide proxy on LUbuntu.
+# Takes two command line arguments:
+# The first is the IP address of the proxy server
+# The second is the PORN number of the proxy server
+if [ $(id -u) -ne 0 ]; then
+  echo "This script must be run as root";
+  exit 1;
+fi
+
+if [ $# -eq 2 ]
+  then
+
+  gsettings set org.gnome.system.proxy mode 'manual' ;
+  gsettings set org.gnome.system.proxy.http host '$1';
+  gsettings set org.gnome.system.proxy.http port $2;
+
+
+  grep PATH /etc/environment > lol.t;
+  printf \
+  "http_proxy=http://$1:$2/\n\
+  https_proxy=http://$1:$2/\n\
+  ftp_proxy=http://$1:$2/\n\
+  no_proxy=\"localhost,127.0.0.1,localaddress,.localdomain.com\"\n\
+  HTTP_PROXY=http://$1:$2/\n\
+  HTTPS_PROXY=http://$1:$2/\n\
+  FTP_PROXY=http://$1:$2/\n\
+  NO_PROXY=\"localhost,127.0.0.1,localaddress,.localdomain.com\"\n" >> lol.t;
+
+  cat lol.t > /etc/environment;
+
+
+  printf \
+  "Acquire::http::proxy \"http://$1:$2/\";\n\
+  Acquire::ftp::proxy \"ftp://$1:$2/\";\n\
+  Acquire::https::proxy \"https://$1:$2/\";\n" > /etc/apt/apt.conf.d/95proxies;
+
+  rm -rf lol.t;
+
+  else
+
+  printf "Usage $0 <proxy_ip> <proxy_port>\n";
+
+fi
