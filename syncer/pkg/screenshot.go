49aa50aface6aa64729c5762f5ebd817712c3a45
mrnakumar <nknkchoudhary@gmail.com>
2022-03-27 11:52:21 +0200

    Move scren shot maker to file screenshot

--- syncer/pkg/screenshot.go
+++ syncer/pkg/screenshot.go
@@ -5,18 +5,67 @@ import (
 	"fmt"
 	"github.com/kbinani/screenshot"
 	"image/png"
+	"io/ioutil"
+	"log"
 	"math/rand"
+	"os"
+	"path"
+	"strings"
+	"sync"
 	"time"
 )
 
-type ScreenShot struct {
+type screenShot struct {
 	Name  string
 	Image []byte
 }
 
-func TakeScreenShot(NamePrefix string) ([]ScreenShot, error) {
+func ScreenShotMaker(wg *sync.WaitGroup, interval uint64, recipientKeyPath string, shotsPath string) {
+	defer wg.Done()
+	if _, err := os.Stat(shotsPath); os.IsNotExist(err) {
+		err := os.Mkdir(shotsPath, 0644)
+		log.Fatalf("failed to create shots path '%s'. Caused by : '%v'", shotsPath, err)
+	}
+	recipientKey, err := ioutil.ReadFile(recipientKeyPath)
+	if err != nil {
+		log.Fatalf("failed to read file '%s'. Caused by : '%v'", recipientKeyPath, err)
+	}
+
+	recipient := string(recipientKey)
+	encryptor, err := CreateEncryptor(strings.TrimSuffix(recipient, "\n"))
+
+	if err != nil {
+		log.Fatalf("failed to create encryptor. Caused by : '%v'", err)
+	}
+	for {
+		shots, err := takeScreenShot("screen")
+		if err != nil {
+			log.Printf("failed to take shot. Caused by: '%v'", err)
+		} else {
+			for _, shot := range shots {
+				encrypted, err := encryptor.Encrypt(shot.Image)
+				if err == nil {
+					shotPathInProgress := path.Join(shotsPath, shot.Name+".progress")
+					err := os.WriteFile(shotPathInProgress, encrypted, 0644)
+					if err != nil {
+						log.Printf("failed to save shot. Caused by: '%v'", err)
+					}
+					err = os.Rename(shotPathInProgress, path.Join(shotsPath, shot.Name))
+					if err != nil {
+						log.Printf("failed to rename shot '%s'. Caused by '%v", shotPathInProgress, err)
+					}
+				} else {
+					log.Printf("failed to encrypt shot '%s'", shot.Name)
+				}
+			}
+		}
+		time.Sleep(time.Second * time.Duration(interval))
+	}
+}
+
+func takeScreenShot(NamePrefix string) ([]screenShot, error) {
 	n := screenshot.NumActiveDisplays()
-	var screenShots []ScreenShot
+	var screenShots []screenShot
 	for i := 0; i < n; i++ {
 		bounds := screenshot.GetDisplayBounds(i)
 		img, err := screenshot.CaptureRect(bounds)
@@ -30,7 +79,7 @@ func TakeScreenShot(NamePrefix string) ([]ScreenShot, error) {
 		}
 		now := time.Now().UnixMilli()
 		name := fmt.Sprintf("%s_%d_%d.png", NamePrefix, now, randomNumber())
-		screenShots = append(screenShots, ScreenShot{Name: name, Image: buffer.Bytes()})
+		screenShots = append(screenShots, screenShot{Name: name, Image: buffer.Bytes()})
 	}
 	return screenShots, nil
 }
--- syncer/syncer.go
+++ syncer/syncer.go
@@ -1,64 +1,16 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
-	"os"
-	"path"
-	"strings"
 	"sync"
 	"syncer/pkg"
-	"time"
 )
 
 func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go screenShotMaker(&wg, 10, "/keys/pub",
+	go pkg.ScreenShotMaker(&wg, 10, "/keys/pub",
 		"/syncer/shots")
 	wg.Wait()
 	log.Println("exiting")
-}
-
-func screenShotMaker(wg *sync.WaitGroup, interval uint64, recipientKeyPath string, shotsPath string) {
-	defer wg.Done()
-	if _, err := os.Stat(shotsPath); os.IsNotExist(err) {
-		err := os.Mkdir(shotsPath, 0644)
-		log.Fatalf("failed to create shots path '%s'. Caused by : '%v'", shotsPath, err)
-	}
-	recipientKey, err := ioutil.ReadFile(recipientKeyPath)
-	if err != nil {
-		log.Fatalf("failed to read file '%s'. Caused by : '%v'", recipientKeyPath, err)
-	}
-
-	recipient := string(recipientKey)
-	encryptor, err := pkg.CreateEncryptor(strings.TrimSuffix(recipient, "\n"))
-
-	if err != nil {
-		log.Fatalf("failed to create encryptor. Caused by : '%v'", err)
-	}
-	for {
-		shots, err := pkg.TakeScreenShot("screen")
-		if err != nil {
-			log.Printf("failed to take shot. Caused by: '%v'", err)
-		} else {
-			for _, shot := range shots {
-				encrypted, err := encryptor.Encrypt(shot.Image)
-				if err == nil {
-					shotPathInProgress := path.Join(shotsPath, shot.Name+".progress")
-					err := os.WriteFile(shotPathInProgress, encrypted, 0644)
-					if err != nil {
-						log.Printf("failed to save shot. Caused by: '%v'", err)
-					}
-					err = os.Rename(shotPathInProgress, path.Join(shotsPath, shot.Name))
-					if err != nil {
-						log.Printf("failed to rename shot '%s'. Caused by '%v", shotPathInProgress, err)
-					}
-				} else {
-					log.Printf("failed to encrypt shot '%s'", shot.Name)
-				}
-			}
-		}
-		time.Sleep(time.Second * time.Duration(interval))
-	}
 }
