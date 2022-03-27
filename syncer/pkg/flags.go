c1c4f5dff48574ba0662cae5e905222c9cca6d27
mrnakumar <nknkchoudhary@gmail.com>
2022-03-27 16:11:09 +0200

    Add base64 decode support for input flags
    Note:- Password is reversed after decoding. If the
    password is 'xyz', then user first reverse it to
    'zyx' and then do base64 encode before providing
    it as input flag.

--- syncer/pkg/flags.go
+++ syncer/pkg/flags.go
@@ -1,6 +1,7 @@
 package pkg
 
 import (
+	"encoding/base64"
 	"flag"
 	"log"
 	"os"
@@ -17,53 +18,157 @@ type ParsedFlag struct {
 	ShotsFolder        string
 }
 
+type flagInfo struct {
+	name         string
+	defaultValue string
+	usage        string
+	userSupplied *string
+	validation   func(info *flagInfo)
+	provide      func(info *flagInfo)
+}
+
 func ParseFlags() ParsedFlag {
-	screenShotIntervalFlag := "cap_interval"
-	fromEmailFlag := "from_email"
-	gateFlag := "gate"
-	toEmailFlag := "to_email"
-	keyPathFlag := "keyPath"
-	shotsFolderFlag := "shots"
-	screenShotInterval := flag.String(screenShotIntervalFlag, "", "Capture interval in seconds. Must be greater than 10. Ex. 22")
-	fromEmail := flag.String(fromEmailFlag, "", "Sender email address")
-	pwd := flag.String(gateFlag, "", "Sender's password")
-	toEmail := flag.String(toEmailFlag, "", "Receiver email address")
-	keyPath := flag.String(keyPathFlag, "", "Recipient's public key path")
-	shotsFolder := flag.String(shotsFolderFlag, "", "Shots folder path")
-
+	var screenShotInterval uint16 = 0
+	var fromEmail string
+	var password string
+	var toEmail string
+	var keyPath string
+	var shotsFolder string
+	flagInfos := []*flagInfo{
+		{
+			name:         "cap_interval",
+			usage:        "Capture interval in seconds. Must be greater than 10. Ex. 22",
+			userSupplied: nil,
+			validation:   checkUnsigned16,
+			provide:      asUin16(&screenShotInterval),
+		},
+		{
+			name:         "from_email",
+			usage:        "Sender email address",
+			userSupplied: nil,
+			validation:   emptyCheck,
+			provide:      asBase64Decode(&fromEmail),
+		},
+		{
+			name:         "to_email",
+			usage:        "Receiver email address",
+			userSupplied: nil,
+			validation:   emptyCheck,
+			provide:      asBase64Decode(&toEmail),
+		},
+		{
+			name:         "gate",
+			usage:        "Sender's password",
+			userSupplied: nil,
+			validation:   emptyCheck,
+			provide:      asPassword(&password),
+		},
+		{
+			name:         "keyPath",
+			usage:        "Recipient's public key path",
+			userSupplied: nil,
+			validation:   checkPathExists,
+			provide:      asBase64Decode(&keyPath),
+		},
+		{
+			name:         "shots",
+			usage:        "Shots folder path",
+			userSupplied: nil,
+			validation:   checkPathExists,
+			provide:      asBase64Decode(&shotsFolder),
+		},
+	}
+	for _, flagInfo := range flagInfos {
+		flagInfo.userSupplied = flag.String(flagInfo.name, flagInfo.defaultValue, flagInfo.usage)
+	}
 	flag.Parse()
-
-	if len(strings.TrimSpace(*fromEmail)) == 0 {
-		log.Fatalf("invalid '%s' '%s'", fromEmailFlag, *fromEmail)
-	}
-	if len(strings.TrimSpace(*toEmail)) == 0 {
-		log.Fatalf("invalid '%s' '%s'", toEmailFlag, *toEmail)
-	}
-	if len(strings.TrimSpace(*pwd)) == 0 {
-		log.Fatalf("invalid '%s' '%s'", gateFlag, *pwd)
-	}
-	if len(strings.TrimSpace(*shotsFolder)) == 0 {
-		log.Fatalf("invalid '%s' '%s'", shotsFolderFlag, *shotsFolder)
-	}
-	if len(strings.TrimSpace(*keyPath)) == 0 {
-		log.Fatalf("invalid '%s' '%s'", keyPathFlag, *keyPath)
-	}
-
-	interval, err := strconv.Atoi(*screenShotInterval)
-	if err != nil || interval <= 10 {
-		log.Fatalf("invalid '%s' '%s'", screenShotIntervalFlag, *screenShotInterval)
-	}
-	if _, err := os.Stat(*shotsFolder); os.IsNotExist(err) {
-		log.Fatalf("invalid '%s' '%s'", screenShotIntervalFlag, *screenShotInterval)
-	}
+	validateAndProvide(flagInfos)
 
 	// todo: add base64 or similar decoding
 	return ParsedFlag{
-		ScreenShotInterval: uint16(interval),
-		FromEmail:          *fromEmail,
-		Password:           *pwd,
-		ToEmail:            *toEmail,
-		KeyPath:            *keyPath,
-		ShotsFolder:        *shotsFolder,
+		ScreenShotInterval: screenShotInterval,
+		FromEmail:          fromEmail,
+		Password:           password,
+		ToEmail:            toEmail,
+		KeyPath:            keyPath,
+		ShotsFolder:        shotsFolder,
 	}
+}
+
+func validateAndProvide(flags []*flagInfo) {
+	for _, flagInfo := range flags {
+		flagInfo.validation(flagInfo)
+		flagInfo.provide(flagInfo)
+	}
+}
+func emptyCheck(input *flagInfo) {
+	if len(strings.TrimSpace(*input.userSupplied)) == 0 {
+		log.Fatalf("invalid '%s' '%s'", input.name, *input.userSupplied)
+	}
+}
+
+func checkUnsigned16(input *flagInfo) {
+	interval, err := strconv.Atoi(*input.userSupplied)
+	if err != nil || interval <= 10 || interval > 65535 {
+		log.Fatalf("invalid '%s' '%s'", input.name, *input.userSupplied)
+	}
+}
+
+func checkPathExists(input *flagInfo) {
+	if _, err := os.Stat(*input.userSupplied); os.IsNotExist(err) {
+		log.Fatalf("invalid '%s' '%s'", input.name, *input.userSupplied)
+	}
+}
+
+func asIs(target *string) func(info *flagInfo) {
+	return func(input *flagInfo) {
+		*target = *input.userSupplied
+	}
+}
+
+func asUin16(target *uint16) func(*flagInfo) {
+	return func(input *flagInfo) {
+		interval, _ := strconv.Atoi(*input.userSupplied)
+		*target = uint16(interval)
+	}
+}
+
+func asBase64Decode(target *string) func(*flagInfo) {
+	return func(input *flagInfo) {
+		*target = decode(input)
+	}
+}
+
+func asPassword(target *string) func(info *flagInfo) {
+	return func(input *flagInfo) {
+		str := decode(input)
+		output := reverse(str)
+		*target = output
+	}
+}
+
+func decode(input *flagInfo) string {
+	decoded, err := base64.StdEncoding.DecodeString(*input.userSupplied)
+	if err != nil {
+		log.Fatalf("failed to decode '%s'", input.name)
+	}
+	return string(decoded)
+}
+
+func reverse(str string) string {
+	// Get Unicode code points.
+	n := 0
+	runes := make([]rune, len(str))
+	for _, r := range str {
+		runes[n] = r
+		n++
+	}
+	runes = runes[0:n]
+	// Reverse
+	for i := 0; i < n/2; i++ {
+		runes[i], runes[n-1-i] = runes[n-1-i], runes[i]
+	}
+	// Convert back to UTF-8.
+	output := string(runes)
+	return output
 }
