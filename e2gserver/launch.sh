e7dbf73901ce3776f0fb7248689b3030ccd0a1c5
mrnakumar <nknkchoudhary@gmail.com>
2022-04-16 17:35:27 +0200

    Add launch scripts for develop mode as a template

---
+++ e2gserver/launch.sh
@@ -0,0 +1,7 @@
+go build server.go && ./server \
+-base_path="L1VzZXJzL25hcmVuZHJhL3dvcmtzcGFjZS9lMmd1YXJkaWFuX3NldHVwL2UyZ3NlcnZlci9wbGF5Z3JvdW5kL2RhdGE=" \
+-domain="bXJuYWt1bWFyLmNvbQ==" \
+-user_name="bmt1bWFy" \
+-password="cmFtdWtu" \
+-identity_file_path="L1VzZXJzL25hcmVuZHJhL3dvcmtzcGFjZS9lMmd1YXJkaWFuX3NldHVwL2UyZ3NlcnZlci9wbGF5Z3JvdW5kL2hlYWRlclByaQ==" \
+
---
+++ syncer/launch.sh
@@ -0,0 +1,10 @@
+go build syncer.go && ./syncer \
+-cap_interval="11" \
+-sync_interval="21" \
+-storage_limit="200" \
+-user_name="bmt1bWFy" \
+-gate="cmFtdWtu" \
+-server_url="aHR0cDovL2xvY2FsaG9zdDo4MDgwL3Nob290" \
+-shot_key_path="L1VzZXJzL25hcmVuZHJhL3dvcmtzcGFjZS9lMmd1YXJkaWFuX3NldHVwL3N5bmNlci9wbGF5Z3JvdW5kL2RhdGFQdWI=" \
+-header_key_path="L1VzZXJzL25hcmVuZHJhL3dvcmtzcGFjZS9lMmd1YXJkaWFuX3NldHVwL2UyZ3NlcnZlci9wbGF5Z3JvdW5kL2hlYWRlclB1Yg==" \
+-shots="L1VzZXJzL25hcmVuZHJhL3dvcmtzcGFjZS9lMmd1YXJkaWFuX3NldHVwL3N5bmNlci9wbGF5Z3JvdW5kL3Nob3Rz"
